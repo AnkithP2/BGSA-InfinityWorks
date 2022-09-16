dec582588b9ba127a045af4e854a92b2f1629383
SeanKelly <seanjosephkelly@tamu.edu>
2022-09-16 23:28:59 +0000

    - Implemented index, show, and form for adding events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,13 +4,29 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
+  # To cover if value on the database have defaults for forms
+  # to fill in
+  # create the form
   def new
     @event = Event.new
   end
 
+  # instantiate the form contents
   def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -21,4 +37,10 @@ class EventsController < ApplicationController
 
   def destroy
   end
+
+  public
+      # Only allow a list of trusted parameters through.
+      def event_params
+        params.require(:event).permit(:title, :date, :starttime, :endtime, :logincode, :location, :eventpoints)
+      end
 end
