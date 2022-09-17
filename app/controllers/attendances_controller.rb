1b716caf90d4232e63b8526a09cdf73d0ea15699
SeanKelly <seanjosephkelly@tamu.edu>
2022-09-17 00:32:44 +0000

    Removed unnecessary changes regarding to Events within attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,22 +1,22 @@
 class AttendancesController < ApplicationController
   def index
-    @attendances = attendance.all
+    @attendances = Attendance.all
   end
 
   def show
-    @attendance = attendance.find(params[:id])
+    @attendance = Attendance.find(params[:id])
   end
 
   # To cover if value on the database have defaults for forms
   # to fill in
   # create the form
   def new
-    @attendance = attendance.new
+    @attendance = Attendance.new
   end
 
   # instantiate the form contents
   def create
-    @attendance = attendance.new(attendance_params)
+    @attendance = Attendance.new(attendance_params)
 
     respond_to do |format|
       if @attendance.save
@@ -30,12 +30,12 @@ class AttendancesController < ApplicationController
   end
 
   def edit
-    @attendance = attendance.find(params[:id])
+    @attendance = Attendance.find(params[:id])
   end
 
   def update
     respond_to do |format|
-        @attendance = attendance.find(params[:id])
+        @attendance = Attendance.find(params[:id])
         if @attendance.update(attendance_params)
           format.html { redirect_to attendance_url(@attendance), notice: "attendance was successfully updated." }
           format.json { render :show, status: :ok, location: @attendance }
@@ -47,11 +47,11 @@ class AttendancesController < ApplicationController
   end
 
   def delete
-    @attendance=attendance.find(params[:id])
+    @attendance=Attendance.find(params[:id])
   end
 
   def destroy
-    @attendance=attendance.find(params[:id])
+    @attendance=Attendance.find(params[:id])
     @attendance.destroy
     flash[:notice]="attendance '#{@attendance.title}' deleted successfully."
     redirect_to(attendances_path)
