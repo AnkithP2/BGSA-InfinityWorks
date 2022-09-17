451f17623347b3b2c9bcf6a9a7e1c499b24aa457
SeanKelly <seanjosephkelly@tamu.edu>
2022-09-17 01:20:04 +0000

    - Added display_attending helper method within attendances helper - Events now shows user id's that are attending - Adding linking between events and attendances view files

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+    def display_attending
+        if(@event.attendances.size == 0)
+            'None'
+        else
+            @event.attendances.map(&:userid).join ", "
+        end
+    end
 end
