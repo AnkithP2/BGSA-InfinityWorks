3acd26d83f71827fe9fc065d831a123337a0a080
root <root@localhost>
2022-09-20 00:11:55 +0000

    Fixed migration issue, and added a link to links page in the events home page. Also Links page also now has a link to the events page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_09_19_024651) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +25,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_024651) do
     t.string "name"
     t.string "email"
     t.string "password"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
