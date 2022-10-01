ab83da06d0c883da85d729032e7d0727943cd7e0
samjefferis <55027164+samjefferis@users.noreply.github.com>
2022-10-01 13:33:32 -0500

    Attendance is messed up

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -1,6 +1,7 @@
 class Mentorship < ApplicationRecord
   has_many :goals
 
-  belongs_to :mentor_id, class_name: 'User'
-  belongs_to :mentee_id, class_name: 'User'
+  belongs_to :mentor_id
+  belongs_to :mentee_id
+  # , class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     validates :firstname, :lastname, :userpoints, :usertotal, presence: true
     has_many :attendances
 
-    has_many :mentee, class_name: 'Mentorship', foreign_key: 'mentee_id'
-    has_many :mentor, class_name: 'Mentorship', foreign_key: 'mentor_id'
+    has_many :mentees, class_name: 'Mentorship', foreign_key: 'mentee_id'
+    has_many :mentors, class_name: 'Mentorship', foreign_key: 'mentor_id'
 end
