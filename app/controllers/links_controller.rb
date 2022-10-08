bf0884987efdd24a84b5f5d829895aee2991eef6
BWSullivan <brandon71399@gmail.com>
2022-10-08 14:17:10 -0500

    current changes
    laptop battery is dead, pushing changes to continue working

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,8 @@
 class LinksController < ApplicationController
   def index
     @links = Link.all
+    @sections = Link.sections
+    @specificsection = Link.specsections
   end
 
   def show
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,7 @@
 class Link < ApplicationRecord
     validates :section, :title, :link, presence: true
+
+    scope :sections, -> {distinct.select(:section)}
+    
+    scope :specsections, -> {where(section:"123")}
 end
