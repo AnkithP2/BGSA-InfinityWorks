cbc39d4f67c8b1925baa1725f76f0dff5d94a1eb
SeanJosephKelly <seanjosephkelly@tamu.edu>
2022-10-09 18:29:22 +0000

    -Switched foreign keys of Users to search for where the supplied ID is equal to the foreign key, so calling mentors,mentees will return the right tables - updated the goals and mentorship controllers to allow creationg, edits, and indexes of these classes - Removed _id_id names, that was bad...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    validates :firstname, :lastname, :userpoints, :usertotal, presence: true
+    #validates :firstname, :lastname, :userpoints, :usertotal, presence: true
     has_many :attendances
 
-    has_many :mentees, class_name: 'Mentorship', foreign_key: 'mentee_id'
-    has_many :mentors, class_name: 'Mentorship', foreign_key: 'mentor_id'
+    #where foreign_key is = to the supplied user id
+    has_many :mentees, class_name: 'Mentorship', foreign_key: 'mentor_id'
+    has_many :mentors, class_name: 'Mentorship', foreign_key: 'mentee_id'
 end
