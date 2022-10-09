bd5dafaa62c72b798b466d30d9a73d0d8bc9dd78
SeanJosephKelly <seanjosephkelly@tamu.edu>
2022-10-09 17:21:36 +0000

    Fixed create_mentorship migration

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -1,7 +1,7 @@
 class Mentorship < ApplicationRecord
   has_many :goals
 
-  belongs_to :mentor_id
-  belongs_to :mentee_id
+  belongs_to :mentor, :class_name => 'User'
+  belongs_to :mentee, :class_name => 'User'
   # , class_name: 'User'
 end
--- db/migrate/20221001174414_create_mentorships.rb
+++ db/migrate/20221001174414_create_mentorships.rb
@@ -1,14 +1,14 @@
 class CreateMentorships < ActiveRecord::Migration[6.1]
   def change
     create_table :mentorships do |t|
-      t.references :mentor_id, null: false, foreign_key: true
-      t.references :mentee_id, null: false, foreign_key: true
+      t.references :mentor
+      t.references :mentee
 
       t.timestamps
     end
 
-    add_foreign_key :mentorships, :users, column: :mentor_id
-    add_foreign_key :mentorships, :users, column: :mentee_id
+    add_foreign_key :mentorships, :users, column: :mentor_id, primary_key: :id
+    add_foreign_key :mentorships, :users, column: :mentee_id, primary_key: :id
 
   end
 end
