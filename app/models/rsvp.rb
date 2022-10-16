85e9b383f0d4ab1d512ce0d0c391947e155e8b2c
root <root@localhost>
2022-10-16 18:19:55 +0000

    Add missing calls to RSVP to models

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,25 @@
 class Rsvp < ApplicationRecord
     belongs_to :event
+    has_many :users
+
+    def self.error_checks(rsvp)
+        errors = []
+        if !check_rsvp_time(rsvp)
+            errors.append("Registration Closed")
+        end
+        if !check_user_exists(rsvp)
+            errors.append("User Already Registered")
+        end
+        return errors
+    end
+    
+    def self.check_user_exists(rsvp)
+        return Rsvp.where(:userid => rsvp.userid, :event_id => rsvp.event_id).empty?
+    end
+    
+    def self.check_rsvp_time(rsvp)
+        start_time = Event.find(rsvp.event_id).starttime
+        current_time = DateTime.now.in_time_zone('US/Central')
+        return (current_time < start_time)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-    validates :firstname, :lastname, :userpoints, :usertotal, presence: true
+    #validates :firstname, :lastname, :userpoints, :usertotal, presence: true
     has_many :attendances
+    has_many :rsvps
+
+    #where foreign_key is = to the supplied user id
+    has_many :mentees, class_name: 'Mentorship', foreign_key: 'mentor_id'
+    has_many :mentors, class_name: 'Mentorship', foreign_key: 'mentee_id'
 end
