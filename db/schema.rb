07be391dd2f2456d0738c6aaafe36fc09c83104a
root <root@localhost>
2022-10-17 23:44:45 +0000

    schema.rb migration file update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_10_14_195933) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,12 +54,11 @@ ActiveRecord::Schema.define(version: 2022_10_14_195933) do
   end
 
   create_table "links", force: :cascade do |t|
-    t.bigint "section_id"
+    t.string "author"
     t.string "title"
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["section_id"], name: "index_links_on_section_id"
   end
 
   create_table "mentorships", force: :cascade do |t|
