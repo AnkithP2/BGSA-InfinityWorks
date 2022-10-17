8a0e07836c22ddde2ad22779f6b2ba002e8589b8
root <root@localhost>
2022-10-17 23:35:12 +0000

    Local commit for goals and mentors

--- db/schema.rb
+++ db/schema.rb
@@ -44,12 +44,30 @@ ActiveRecord::Schema.define(version: 2022_10_12_003559) do
     t.datetime "endtime"
   end
 
+  create_table "goals", force: :cascade do |t|
+    t.bigint "mentorship_id"
+    t.string "goal"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mentorship_id"], name: "index_goals_on_mentorship_id"
+  end
+
   create_table "links", force: :cascade do |t|
     t.string "author"
     t.string "title"
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mentorships", force: :cascade do |t|
+    t.bigint "mentor_id"
+    t.bigint "mentee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mentee_id"], name: "index_mentorships_on_mentee_id"
+    t.index ["mentor_id"], name: "index_mentorships_on_mentor_id"
   end
 
   create_table "rsvps", force: :cascade do |t|
@@ -67,26 +85,6 @@ ActiveRecord::Schema.define(version: 2022_10_12_003559) do
     t.integer "usertotal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-
-  create_table "goals", force: :cascade do |t|
-    t.bigint "mentorship_id"
-    t.string "goal"
-    t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["mentorship_id"], name: "index_goals_on_mentorship_id"
-  end
-
-
-  create_table "mentorships", force: :cascade do |t|
-    t.bigint "mentor_id"
-    t.bigint "mentee_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["mentee_id"], name: "index_mentorships_on_mentee_id"
-    t.index ["mentor_id"], name: "index_mentorships_on_mentor_id"
   end
 
   add_foreign_key "mentorships", "users", column: "mentee_id"
