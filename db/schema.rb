0d35f578d1306c6b3c85a99fc392e30964e7a8e7
root <root@localhost>
2022-10-17 04:00:12 +0000

    Updated attendance view

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_024651) do
+ActiveRecord::Schema.define(version: 2022_10_12_003559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_securities", force: :cascade do |t|
-    t.date "last_login"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -42,13 +35,22 @@ ActiveRecord::Schema.define(version: 2022_09_19_024651) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.date "date"
-    t.time "starttime"
-    t.time "endtime"
     t.string "logincode"
     t.string "location"
     t.integer "eventpoints"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "starttime"
+    t.datetime "endtime"
+  end
+
+  create_table "goals", force: :cascade do |t|
+    t.bigint "mentorship_id"
+    t.string "goal"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mentorship_id"], name: "index_goals_on_mentorship_id"
   end
 
   create_table "links", force: :cascade do |t|
@@ -57,6 +59,15 @@ ActiveRecord::Schema.define(version: 2022_09_19_024651) do
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mentorships", force: :cascade do |t|
+    t.bigint "mentor_id"
+    t.bigint "mentee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mentee_id"], name: "index_mentorships_on_mentee_id"
+    t.index ["mentor_id"], name: "index_mentorships_on_mentor_id"
   end
 
   create_table "rsvps", force: :cascade do |t|
@@ -76,4 +87,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_024651) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "mentorships", "users", column: "mentee_id"
+  add_foreign_key "mentorships", "users", column: "mentor_id"
 end
