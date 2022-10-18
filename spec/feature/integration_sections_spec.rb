0168e6dfd8a14411a581a4e2ab4b6f82f5857763
BWSullivan <brandon71399@gmail.com>
2022-10-17 21:43:35 -0500

    Added links and sections integration test cases

---
+++ spec/feature/integration_sections_spec.rb
@@ -0,0 +1,17 @@
+# location: spec/feature/integration_links_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a link: ', type: :feature do
+  scenario 'valid inputs' do
+    visit new_registration_path
+    fill_in 'Name', with: 'Brandon'
+    fill_in 'Email', with: 'test@gmail.com'
+    fill_in 'Password', with: '12345'
+    click_on 'Create Admin'
+    visit new_section_path
+    fill_in 'Label', with: 'newsection'
+    click_on 'Create Section'
+    visit sections_path
+    expect(page).to have_content('newsection')
+  end
+end
