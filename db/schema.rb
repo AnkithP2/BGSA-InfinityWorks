5cab975cf443acc8fa3a205486be9a7235cffac7
samjefferis <55027164+samjefferis@users.noreply.github.com>
2022-10-20 19:54:21 -0500

    Book fix and admin password fields
    removed associated book & password fields are now protected

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,89 +10,90 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_221_014_195_933) do
+ActiveRecord::Schema.define(version: 2022_10_14_195933) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'admins', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'attendances', force: :cascade do |t|
-    t.bigint 'event_id'
-    t.integer 'userid'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password'
-    t.index ['event_id'], name: 'index_attendances_on_event_id'
+  create_table "attendances", force: :cascade do |t|
+    t.bigint "event_id"
+    t.integer "userid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
+    t.index ["event_id"], name: "index_attendances_on_event_id"
   end
 
-  create_table 'events', force: :cascade do |t|
-    t.string 'title'
-    t.date 'date'
-    t.string 'logincode'
-    t.string 'location'
-    t.integer 'eventpoints'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.datetime 'starttime'
-    t.datetime 'endtime'
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.string "logincode"
+    t.string "location"
+    t.integer "eventpoints"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "starttime"
+    t.datetime "endtime"
   end
 
-  create_table 'goals', force: :cascade do |t|
-    t.bigint 'mentorship_id'
-    t.string 'goal'
-    t.string 'status'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['mentorship_id'], name: 'index_goals_on_mentorship_id'
+  create_table "goals", force: :cascade do |t|
+    t.bigint "mentorship_id"
+    t.string "goal"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mentorship_id"], name: "index_goals_on_mentorship_id"
   end
 
-  create_table 'links', force: :cascade do |t|
-    t.bigint 'section_id'
-    t.string 'title'
-    t.string 'link'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['section_id'], name: 'index_links_on_section_id'
+  create_table "links", force: :cascade do |t|
+    t.bigint "section_id"
+    t.string "title"
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["section_id"], name: "index_links_on_section_id"
   end
 
-  create_table 'mentorships', force: :cascade do |t|
-    t.bigint 'mentor_id'
-    t.bigint 'mentee_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['mentee_id'], name: 'index_mentorships_on_mentee_id'
-    t.index ['mentor_id'], name: 'index_mentorships_on_mentor_id'
+  create_table "mentorships", force: :cascade do |t|
+    t.bigint "mentor_id"
+    t.bigint "mentee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mentee_id"], name: "index_mentorships_on_mentee_id"
+    t.index ["mentor_id"], name: "index_mentorships_on_mentor_id"
   end
 
-  create_table 'rsvps', force: :cascade do |t|
-    t.bigint 'event_id'
-    t.integer 'userid'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['event_id'], name: 'index_rsvps_on_event_id'
+  create_table "rsvps", force: :cascade do |t|
+    t.bigint "event_id"
+    t.integer "userid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_rsvps_on_event_id"
   end
 
-  create_table 'sections', force: :cascade do |t|
-    t.string 'label'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "sections", force: :cascade do |t|
+    t.string "label"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'firstname'
-    t.string 'lastname'
-    t.integer 'userpoints'
-    t.integer 'usertotal'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "userpoints"
+    t.integer "usertotal"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'mentorships', 'users', column: 'mentee_id'
-  add_foreign_key 'mentorships', 'users', column: 'mentor_id'
+  add_foreign_key "mentorships", "users", column: "mentee_id"
+  add_foreign_key "mentorships", "users", column: "mentor_id"
 end
