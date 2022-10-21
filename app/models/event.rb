3a56be4d7cc1dc57607f33719130b51413a8bcbd
root <root@localhost>
2022-10-21 01:12:32 +0000

    Fixed variable naming issue and date picker issue for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@
 
 class Event < ApplicationRecord
   # provides a one-to-many relationship with attendence
-  validates :title, :date, :starttime, :endtime, :logincode, :location, :eventpoints, presence: true
+  validates :title, :starttime, :endtime, :logincode, :location, :eventpoints, presence: true
   has_many :attendances
   has_many :rsvps
 end
