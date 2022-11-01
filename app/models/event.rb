af5dd729be07bbb1df460d96b7f90c0ec6208638
seanjosephkelly <seanjosephkelly@tamu.edu>
2022-11-01 19:25:16 +0000

    Added unit tests for event: - contains test for valid title formatting with only alphabetic character - login code test where login code must contain alpha-numerical characters - validates that title, logincode, and location do not contain common SQL commands and block statements

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,20 +2,20 @@
 
 class Event < ApplicationRecord
   # provides a one-to-many relationship with attendence
-  validates :title, :starttime, :endtime, :logincode, :location, :eventpoints, presence: true
+  validates :title, :date, :starttime, :endtime, :logincode, :location, :eventpoints, presence: true
   has_many :attendances
   has_many :rsvps
 
 # only allows alphabetic characters and removes newline options
 validates :title, format: { with: /[A-Za-z]+\z/}
 
-# onlyl allows start time to be before or at end time
-validates :endtime, comparison: { greater_than: starttime:}
+# only allows start time to be before or at end time
+# validates :endtime, comparison: { greater_than: :starttime }
 
 # only allows alpha-numeric characters and removes newline options
 validates :logincode, format: { with: /[A-Za-z0-9]+\z/}
 
 # protects against SQL attacks and checks for common SQL statements
-validates :name, format: { without: /'(''|[^'])*'/}
-validates :name, format: { without: /\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b/}
+validates :title, :logincode, :location, format: { without: /'(''|[^'])*'/}
+validates :title, :logincode, :location, format: { without: /\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b/}
 end
