edc460e7211abd8f3c95bd3dd14ee9bdee2e6d50
seanjosephkelly <seanjosephkelly@tamu.edu>
2022-11-01 18:51:15 +0000

    Added multiple model checks: - Only allows title to have alphabetic characters - start time supplied must now be before end time - login codes are only allowed alpha-numerical characters - general SQL statements and block statement checks added

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,17 @@ class Event < ApplicationRecord
   validates :title, :starttime, :endtime, :logincode, :location, :eventpoints, presence: true
   has_many :attendances
   has_many :rsvps
+
+# only allows alphabetic characters and removes newline options
+validates :title, format: { with: /[A-Za-z]+\z/}
+
+# onlyl allows start time to be before or at end time
+validates :endtime, comparison: { greater_than: starttime:}
+
+# only allows alpha-numeric characters and removes newline options
+validates :logincode, format: { with: /[A-Za-z0-9]+\z/}
+
+# protects against SQL attacks and checks for common SQL statements
+validates :name, format: { without: /'(''|[^'])*'/}
+validates :name, format: { without: /\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b/}
 end
