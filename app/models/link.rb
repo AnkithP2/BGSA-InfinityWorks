38f46ac97ff40f2c2c2ad22f64c625e3cba1b595
seanjosephkelly <seanjosephkelly@tamu.edu>
2022-11-01 20:11:47 +0000

    Added validation of data within links model - Only allows title to have alphabetic characters - Only allows title and link to not have SQL commands and block commands

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,11 +6,8 @@ class Link < ApplicationRecord
 
   # only allows alphabetic characters and removes newline options
   validates :title, format: { with: /[A-Za-z]+\z/}
-  
-  # only allows alpha-numeric characters and removes newline options
-  validates :section_id, format: { with: /[A-Za-z0-9]+\z/}
 
   # protects against SQL attacks and checks for common SQL statements
-  validates :section_id, :title, :link, format: { without: /'(''|[^'])*'/}
-  validates :section_id, :title, :link, format: { without: /\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b/}
+  validates :title, :link, format: { without: /'(''|[^'])*'/}
+  validates :title, :link, format: { without: /\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b/}
 end
