a401239d82a3fc765e234c96a03d224b66298e4f
root <root@localhost>
2022-11-04 17:37:40 +0000

    Created controller, view and route for FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :links
   resources :sections
 
+  resources :faq
+
   resources :admins, only: %i[new create edit update show destroy]
   resources :admin
   resources :registrations
