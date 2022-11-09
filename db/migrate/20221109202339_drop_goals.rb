60d79bfa9fe28a4e4121ba5d2bdbad3e4e04912a
seanjosephkelly <seanjosephkelly@tamu.edu>
2022-11-09 20:32:48 +0000

    dropped mentorship and goals tables using generated migrations

---
+++ db/migrate/20221109202339_drop_goals.rb
@@ -0,0 +1,10 @@
+class DropGoals < ActiveRecord::Migration[6.1]
+  def up
+    drop_table :goals
+  end
+
+  # Once we drop a table we won't be able to create it
+  def down
+    raise ActiveRecrod::IrreversibleMigration
+  end
+end
