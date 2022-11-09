efcaaeea2f77b296ef0246d8a3d20f80e0237f4e
seanjosephkelly <seanjosephkelly@tamu.edu>
2022-11-09 20:49:00 +0000

    Removed mentor and mentee functions within user_helper Removed call to said functions within user index file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,29 +2,4 @@
 
 # Helper for users
 module UsersHelper
-  def list_mentors
-    if @user.mentors.size.zero?
-      "None"
-    else
-      @mentors = @user.mentors.map(&:mentor_id)
-      @mentors.each do |p|
-        @out = "#{@out} #{User.find(p).firstname}"
-      end
-      @out
-      # @user.mentors.map(&:mentor_id).join ", "
-    end
-  end
-
-  def list_mentees
-    if @user.mentees.size.zero?
-      "None"
-    else
-      @mentees = @user.mentees.map(&:mentee_id)
-      @mentees.each do |p|
-        @out = "#{@out} #{User.find(p).firstname}"
-      end
-      @out
-      # @user.mentees.map(&:mentee_id).join ", "
-    end
-  end
 end
