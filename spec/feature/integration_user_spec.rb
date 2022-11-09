4b4a8c32222166d9e9092c9053563d5d61383afd
seanjosephkelly <seanjosephkelly@tamu.edu>
2022-11-09 01:54:50 +0000

    Added integration tests for user: - Added valid and invalid inputs integration tests - Added editing a valid and invalid user integration tests - Added deleting and destroying a valid and invalid user

--- spec/feature/integration_user_spec.rb
+++ spec/feature/integration_user_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-'''
 require "rails_helper"
 
 RSpec.describe "creating an user: ", type: :feature do
@@ -14,47 +13,57 @@ RSpec.describe "creating an user: ", type: :feature do
 
     expect(page).to have_content("John")
   end
-end
 
-RSpec.describe "creating an user: ", type: :feature do
-  scenario "valid inputs" do
+  scenario "invalid username with number" do
     visit new_user_path
-    fill_in "Firstname", with: "John"
+    fill_in "Firstname", with: "John1"
     fill_in "Lastname", with: "Smith"
     fill_in "Userpoints", with: 23
     fill_in "Usertotal", with: 26
     click_on "Create User"
     visit users_path
 
-    expect(page).to have_content("Smith")
+    expect(page).not_to have_content("John")
   end
 end
 
-RSpec.describe "creating an user: ", type: :feature do
-  scenario "valid inputs" do
-    visit new_user_path
-    fill_in "Firstname", with: "John"
-    fill_in "Lastname", with: "Smith"
-    fill_in "Userpoints", with: 23
-    fill_in "Usertotal", with: 26
-    click_on "Create User"
-    visit users_path
+RSpec.describe "editing a user: ", type: :feature do
+    scenario "Edit a user with correct input" do
+      user = User.create!(firstname: 'John', lastname: 'Smith', userpoints: 14, usertotal: 20)
+      visit edit_user_path(:id => user)
+      fill_in "Firstname", with: "Sean"
+      click_on "Update User"
+      
+      visit users_path
+      expect(page).to have_content("Sean")
+    end
 
-    expect(page).to have_content(23)
+    scenario "Edit a user with invalid input" do
+      user = User.create!(firstname: 'John', lastname: 'Smith', userpoints: 14, usertotal: 20)
+      visit edit_user_path(:id => user)
+      fill_in "Firstname", with: "Sean1"
+      click_on "Update User"
+      
+      visit users_path
+      expect(page).not_to have_content("Sean")
+    end
   end
-end
 
-RSpec.describe "creating an user: ", type: :feature do
-  scenario "valid inputs" do
-    visit new_user_path
-    fill_in "Firstname", with: "John"
-    fill_in "Lastname", with: "Smith"
-    fill_in "Userpoints", with: 23
-    fill_in "Usertotal", with: 26
-    click_on "Create User"
-    visit users_path
+RSpec.describe "deleting a user: ", type: :feature do
+    scenario "delete an existing user" do
+      user = User.create!(firstname: 'John', lastname: 'Smith', userpoints: 14, usertotal: 20)
+      visit users_path
+      click_on "Destroy"
+      expect(page).not_to have_content("John")
+    end
 
-    expect(page).to have_content(26)
+    scenario "delete an existing user" do
+      user = User.create!(firstname: 'John', lastname: 'Smith', userpoints: 14, usertotal: 20)
+      visit delete_user_path(:id => user)
+    end
+
+    scenario "Attempt to delete and invalid usear" do
+      visit users_path
+      expect(page).not_to have_content("Destroy")
+    end
   end
-end
-'''
