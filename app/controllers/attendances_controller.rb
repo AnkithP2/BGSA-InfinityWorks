40a815afd581443e0a66264a5d27ed52751407a7
root <root@localhost>
2022-11-11 01:10:36 +0000

    added admin functionality to rsvp and admin

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,11 +3,21 @@
 # This controller controls attendance CRUD and references
 class AttendancesController < ApplicationController
   def index
-    @attendances = Attendance.all
+    if session[:admin_email]
+      @attendances = Attendance.all
+    else
+      message = 'You need admin permissions'
+      redirect_to login_path , notice: message
+    end
   end
 
   def show
-    @attendance = Attendance.find(params[:id])
+    if session[:admin_email]
+      @attendance = Attendance.find(params[:id])
+    else
+      message = 'You need admin permissions'
+      redirect_to login_path , notice: message
+    end
   end
 
   # To cover if value on the database have defaults for forms
@@ -50,31 +60,51 @@ class AttendancesController < ApplicationController
   end
 
   def edit
-    @attendance = Attendance.find(params[:id])
+    if session[:admin_email]
+      @attendance = Attendance.find(params[:id])
+    else
+      message = 'You need admin permissions'
+      redirect_to login_path , notice: message
+    end
   end
 
   def update
-    respond_to do |format|
-      @attendance = Attendance.find(params[:id])
-      if @attendance.update(attendance_params)
-        format.html { redirect_to attendance_url(@attendance), notice: 'attendance was successfully updated.' }
-        format.json { render :show, status: :ok, location: @attendance }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @attendance.errors, status: :unprocessable_entity }
+    if session[:admin_email]
+      respond_to do |format|
+        @attendance = Attendance.find(params[:id])
+        if @attendance.update(attendance_params)
+          format.html { redirect_to attendance_url(@attendance), notice: 'attendance was successfully updated.' }
+          format.json { render :show, status: :ok, location: @attendance }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @attendance.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      message = 'You need admin permissions'
+      redirect_to login_path , notice: message
     end
   end
 
   def delete
-    @attendance = Attendance.find(params[:id])
+    if session[:admin_email]
+      @attendance = Attendance.find(params[:id])
+    else
+      message = 'You need admin permissions'
+      redirect_to login_path , notice: message
+    end
   end
 
   def destroy
-    @attendance = Attendance.find(params[:id])
-    @attendance.destroy
-    flash[:notice] = "attendance '#{@attendance.id}' deleted successfully."
-    redirect_to(attendances_path)
+    if session[:admin_email]
+      @attendance = Attendance.find(params[:id])
+      @attendance.destroy
+      flash[:notice] = "attendance '#{@attendance.id}' deleted successfully."
+      redirect_to(attendances_path)
+    else
+      message = 'You need admin permissions'
+      redirect_to login_path , notice: message
+    end
   end
 
   private
